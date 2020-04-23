a098de1af4d06baf3a7ba9fb641778b0b3e65678
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-04-22 22:29:38 -0700

    added state

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,17 +6,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hello Apama developer ",
+      monsters: [{name: 'Frankestein' ,id: 'sh1', id: 'sh2'}, {name: 'Zombie', id: 'sh3'}],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick ={() => this.setState({string:'to nabeghei va movafaghtarin khahi shod'})}>change the text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => <h1 key= {monster.id}>{monster.name}</h1>)
+        }
       </div>
     );
   }
